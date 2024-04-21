7ecbc42984cfecf28b63dd9eae6cd01c360f64e6
Luis Franco <99168105+Blaxery@users.noreply.github.com>
2024-04-21 21:02:56 +0000

    se modifico archivo

--- src/models.py
+++ src/models.py
@@ -1,32 +1,50 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Table  
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+favoritos_usuario = Table('favoritos_usuario',
+                          Base.metadata,
+                          Column('usuario_id', Integer, ForeignKey('usuario.id')),
+                          Column('favorito_id', Integer, ForeignKey('favorito.id'))
+                         )
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Usuario(Base):
+    __tablename__ = 'usuario'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    email = Column(String(250))
+    usuario = Column(String(250))
+    contraseña = Column(String(250))
+    nombre = Column(String(250), nullable=False)
+    favoritos = relationship("Favorito", secondary=favoritos_usuario, back_populates="usuarios")
 
-    def to_dict(self):
-        return {}
+class Favorito(Base):
+    __tablename__ = 'favorito'
+    id = Column(Integer, primary_key=True)
+    tipo = Column(String(50)) 
+    id_starwars = Column(Integer) 
+    usuarios = relationship("Usuario", secondary=favoritos_usuario, back_populates="favoritos")
+
+class Planeta(Base):
+    __tablename__ = 'planeta'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250))
+    clima = Column(String(250))
+    terreno = Column(String(250))
+    favoritos = relationship("Favorito", back_populates="planetas")
+
+class Personaje(Base):
+    __tablename__ = 'personaje'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250))
+    especie = Column(String(250))
+    genero = Column(String(250))
+    favoritos = relationship("Favorito", back_populates="personajes")
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
+
